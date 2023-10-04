f2e7491dfb7c73111cb231282a25cb623e70327f
Ludwig <dludwig@cps.k12.il.us>
2023-10-04 08:53:10 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#data abstracion- when you need to simplify data 
+#from something more complex
+list1 = []
+print(list1)
+# an example of data abstraction is when I use a list to simplify data instead of having to write # out the same code over and over again
+# data abstraction is when you have to simplify data
+# if you have a list of names, you can simplify it by using a list
